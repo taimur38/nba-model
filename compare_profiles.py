51611bf5e0e39f498385f58dd5b86b0dfe2d79b1
Taimur Shah <taimur38@gmail.com>
2016-01-11 01:36:36 -0500

    more predicty

--- compare_profiles.py
+++ compare_profiles.py
@@ -8,13 +8,45 @@ with open('generated/defense-profile.json') as f:
 with open('generated/shot-profile.json') as f:
 	shot_profile = json.load(f)
 
-team = shot_profile[sys.argv[1]]
 
-ranking = {}
-for coord in team:
-	stat = team[coord]
-	ranking[coord] = stat['made'] + stat['attempt']
+def normalize(team):
 
-sorted_coords = sorted(ranking.items(), key=operator.itemgetter(1))
+	normalized = {}
+	total = 0
+	maximum = -1
+	for coord, stat in team.items():
+		combined = stat['made'] + stat['attempt']
+		normalized[coord] = combined
+		total += combined
+		maximum = maximum if combined < maximum else combined
 
-print(json.dumps(sorted_coords, indent=4, separators=(',', ': ')))
+	for coord in normalized:
+		normalized[coord] /= total
+
+	return normalized
+
+
+def compute_diff(shot_profile, defense_profile):
+	diffs = {}
+	diff = 0
+	for coord in shot_profile:
+		diffs[coord] = shot_profile[coord] + defense_profile.get(coord, 0)
+		diff += diffs[coord]
+
+	return diff, diffs
+
+
+team1_offense = normalize(shot_profile[sys.argv[1]])
+team2_offense = normalize(shot_profile[sys.argv[2]])
+
+team1_defense = normalize(defense_profile[sys.argv[1]])
+team2_defense = normalize(defense_profile[sys.argv[2]])
+
+diff1, _ = compute_diff(team1_offense, team2_defense)
+diff2, _ = compute_diff(team2_offense, team1_defense)
+
+winner = sys.argv[1] if diff1 > diff2 else sys.argv[2]
+print("predicted winner: {winner}".format(winner=winner))
+
+print(sys.argv[1] + " score: " + str(diff1))
+print(sys.argv[2] + " score: " + str(diff2))
